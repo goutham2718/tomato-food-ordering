750716b4d28693068934434e487668587b0e9621
Goutham G <goutham2718@gmail.com>
2026-08-31 16:55:45 +0530

    Updated frontend url in backend folder

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -7,7 +7,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
 //placing user order from frontend
 export const placeOrder = async(req,res)=>{
 
-    const frontend_url = "http://localhost:5173";
+    const frontend_url = "https://tomato-food-ordering-frontend-nu.vercel.app/";
 
     try {
         const newOrder = new orderModel({
